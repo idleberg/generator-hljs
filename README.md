3b985e9aa0e4dcccdeb81ebeea4d145d187490a8
idleberg <jan@idleberg.com>
2018-09-27 23:56:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# generator-hljs
+
+[![npm](https://flat.badgen.net/npm/license/generator-hljs)](https://www.npmjs.org/package/generator-hljs)
+[![npm](https://flat.badgen.net/npm/v/generator-hljs)](https://www.npmjs.org/package/generator-hljs)
+[![CircleCI](https://flat.badgen.net/circleci/github/idleberg/generator-hljs)](https://circleci.com/gh/idleberg/generator-hljs/)
+[![David](https://flat.badgen.net/david/dev/idleberg/generator-hljs)](https://david-dm.org/idleberg/generator-hljs)
+
+## Description
+
+A [Yeoman](http://yeoman.io/authoring/user-interactions.html) generator for Highlight.js
+
+## Prerequisites
+
+You need [Node.js](https://nodejs.org/en/) installed and available in your `PATH` [environmental variable](http://superuser.com/a/284351/195953). Use your preferred Node package manager to install the Yeoman CLI tool.
+
+```sh
+yarn global add yo || npm install -g yo
+```
+
+## Installation
+
+ Use your preferred package manager to install this generator
+
+```sh
+yarn global add generator-hljs || npm i generator-hljs -g
+```
+
+## Usage
+
+Run the generator and follow its instructions. Use `--help` for available flags.
+
+```sh
+yo hljs
+```
+
+*“That's all Folks!”*
+
+## License
+
+This work is licensed under the [MIT License](https://opensource.org/licenses/MIT)
---
+++ generators/app/index.js
@@ -0,0 +1,71 @@
+const Generator = require('yeoman-generator');
+const pkg = require('../../package.json');
+
+const globby = require('globby');
+const updateNotifier = require('update-notifier');
+const yosay = require('yosay');
+const { basename, join } = require('path');
+
+// Is there a newer version of this generator?
+updateNotifier({ pkg: pkg }).notify();
+
+const hljsDir = join(__dirname, '..', '..', 'node_modules', 'highlight.js');
+
+function getLanguages() {
+  const languagesPath = join(hljsDir, 'src', 'languages');
+
+  return globby(`${languagesPath}/*.js`).then( longPaths => {
+    const languages = longPaths.map( longPath => {
+      return basename(longPath, '.js');
+    })
+
+    return languages;
+  });
+}
+
+module.exports = class extends Generator {
+  inquirer() {
+    console.log(yosay('Let me help you build your custom version of Highlight.js'));
+
+    return this.prompt([
+    {
+      name: 'languages',
+      message: 'Select languages to include in your build',
+      type: 'checkbox',
+      choices: () => getLanguages(),
+      validate: (choices) => choices.length > 0 ? true : 'Please select at least one language',
+      store: true
+    },
+    {
+      name: 'auditFix',
+      message: 'Install any compatible updates to vulnerable dependencies',
+      type: 'confirm',
+      store: true
+    }
+    ]).then(props => {
+      const opts = {
+        cwd: hljsDir
+      };
+
+      const npmInstall = this.spawnCommandSync('npm', ['install'], opts);
+      if (npmInstall.status !== 0) throw new Error(`npm install exited with error code ${npmInstall.status}`);
+
+      if (props.auditFix) {
+        const npmAudit = this.spawnCommandSync('npm', ['audit', 'fix'], opts);
+        if (npmAudit.status !== 0) throw new Error(`npm audit exited with error code ${npmAudit.status}`);
+      }
+
+      const args = [`${hljsDir}/tools/build.js`, ...props.languages];
+      const buildCommand = this.spawnCommandSync('node', args, opts);
+      if (buildCommand.status !== 0) throw new Error(`npm install exited with error code ${npmInstall.status}`);
+
+      const sourceFile = `${join(hljsDir, 'build')}/highlight.pack.js`;
+      const targetFile = `${process.cwd()}/highlight.pack.js`;
+
+      this.fs.copy(
+        sourceFile,
+        targetFile
+      );
+    });
+  }
+};
